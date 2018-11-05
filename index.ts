0507d04957cae77716d4cd2766e75fc1aaf6aadc
asad umar <seric.solutions@gmail.com>
2018-11-05 16:32:31 +0500

    simple spelling fixed

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export {MathService} from './src/app/math.service';
-export {SimpModModule} from './src/app/simp-mod/simp-mod.module';
+export {SimpModule} from './src/app/samp/samp.module';
 export {SimpComponent} from './src/app/simp/simp.component';
